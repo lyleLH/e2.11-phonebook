0c911e92719c205c3fa4d0f6079be7aafd1f2b9c
Major Tom <v2top1@163.com>
2023-03-14 02:42:02 +0800

    phonebook json-server, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [persons, setPersons] = useState([]);
+
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise",response.data)
+      setPersons(response.data)
+    });
+  };
+  useEffect(hook, []);
+
+  const [newName, setNewName] = useState("new name");
+  const [newNumber, setNewNumber] = useState("new number");
+  const [keyword, setKeyword] = useState("");
+
+  const newNameInput = (event: { target: { value: any } }) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const newNumberInput = (event: any) => {
+    setNewNumber(event.target.value);
+  };
+
+  const keywordsInput = (event: any) => {
+    const tKeyword = event.target.value;
+    setKeyword(tKeyword);
+  };
+
+  const filteredPersons = (keywordIn: string) => {
+    const newPersons: any[] = [];
+    persons.forEach((person:any) => {
+      let containsTag = person.name.includes(keywordIn);
+      if (containsTag) newPersons.push(person);
+    });
+    console.log("newperson", newPersons);
+    return newPersons;
+  };
+
+  const submitNewPerson = (event: any) => {
+    event.preventDefault();
+
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    console.log(persons);
+    let containsTemp = false;
+    persons.forEach((person:any) => {
+      if (person.name === newName) {
+        alert(`${newName} is already added to phonebook`);
+        containsTemp = true;
+      }
+    });
+
+    if (containsTemp) {
+    } else {
+      setPersons(persons.concat(newPerson as any));
+      setNewName("");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={keyword} onChange={keywordsInput}></input>
+      </div>
+
+      <h2>add a new </h2>
+      <ul>
+        <form onSubmit={submitNewPerson}>
+          <div>
+            name : <input value={newName} onChange={newNameInput}></input>
+          </div>
+
+          <div>
+            number: <input value={newNumber} onChange={newNumberInput}></input>
+          </div>
+
+          <div>
+            <button type="submit"> add</button>
+          </div>
+        </form>
+      </ul>
+
+      <h2>Numbers</h2>
+      <ul>
+        {filteredPersons(keyword).map((person) => (
+          <p>
+            {person.name} {person.number}
+          </p>
+        ))}
+      </ul>
     </div>
   );
 }
